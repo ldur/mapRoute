4d5f68e9faa103b36582242784e7416adf76486a
ldur <lasse@durucz.com>
2023-10-17 11:06:47 +0200

    Initial Commit

---
+++ mapRoute/mapRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapRouteApp.swift
+//  mapRoute
+//
+//  Created by Lasse Durucz on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct mapRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
