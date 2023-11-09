8d396c3118105e08a35622f6155580d6ef5ac4c9
ldur <lasse@durucz.com>
2023-11-09 15:22:32 +0100

    first code init

--- mapRoute/ContentView.swift
+++ mapRoute/ContentView.swift
@@ -6,19 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
+
+
 
 struct ContentView: View {
+  
+    @State private var selectedTag: Int?
+
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(selection: $selectedTag) {
+            Marker("Empire state building", coordinate: .empireStateBuilding)
+                .tint(.orange)
+                .tag(1)
+            Annotation("Columbia University", coordinate: .columbiaUniversity) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(Color.teal)
+                    Text("🎓")
+                        .padding(5)
+                }
+            }
         }
-        .padding()
+        .tag(2)
+        .mapStyle(.standard(elevation: .realistic))
+        .mapControls {
+            MapUserLocationButton()
+            MapCompass()
+            MapScaleView()
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+extension CLLocationCoordinate2D {
+    static let weequahicPark = CLLocationCoordinate2D(latitude: 40.7063, longitude: -74.1973)
+    static let empireStateBuilding = CLLocationCoordinate2D(latitude: 40.7484, longitude: -73.9857)
+    static let columbiaUniversity = CLLocationCoordinate2D(latitude: 40.8075, longitude: -73.9626)
 }
